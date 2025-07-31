81a2e215b4c617e0fefd8dd5cd42fdc806208a25
Lucas Cotta <lcotta00@gmail.com>
2025-07-31 00:25:03 +0000

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import './index.css';
 function App() {
   return (
     <div className="app-container">
-      <h1>Flashcards React</h1>
+      <h1>Flashcards de React</h1>
       <div className="card-grid">
         {questions.map((q, i) => (
           <FlashCard key={i} question={q.question} answer={q.answer} />
